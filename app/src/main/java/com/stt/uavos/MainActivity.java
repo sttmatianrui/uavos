8638512e99bda9828bed3e510b7a17d66023b906
fengpy <fengpeiyun@safetytech.cn>
2017-07-10 15:54:28 +0800

    nonthing

--- app/src/main/java/com/stt/uavos/MainActivity.java
+++ app/src/main/java/com/stt/uavos/MainActivity.java
@@ -1,167 +1,1 @@
-package com.stt.uavos;
-
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.CameraUpdateFactory;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.LatLng;
-import com.amap.api.maps2d.model.MarkerOptions;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMap.OnMapClickListener{
-    protected static final String TAG = "MainActivity";
-
-    private MapView mapView;
-    private AMap aMap;
-
-    private Button locate, add, clear;
-    private Button config, upload, start, stop;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    public void onReturn(View view) {
-        Log.d(TAG, "onReturn");
-        this.finish();
-    }
-
-    private void initUI() {
-        locate = (Button) findViewById(R.id.locate);
-        add = (Button) findViewById(R.id.add);
-        clear = (Button) findViewById(R.id.clear);
-        config = (Button) findViewById(R.id.config);
-        upload = (Button) findViewById(R.id.upload);
-        start = (Button) findViewById(R.id.start);
-        stop = (Button) findViewById(R.id.stop);
-        locate.setOnClickListener(this);
-        add.setOnClickListener(this);
-        clear.setOnClickListener(this);
-        config.setOnClickListener(this);
-        upload.setOnClickListener(this);
-        start.setOnClickListener(this);
-        stop.setOnClickListener(this);
-    }
-
-    private void initMapView() {
-        if(aMap == null) {
-            aMap = mapView.getMap();
-            aMap.setOnMapClickListener(this);
-        }
-        LatLng beijing = new LatLng(40.10080756, 116.37034178);//高德地图：116.37034178；40.10080756// 百度地图：40.106683, 116.376924
-        aMap.addMarker(new MarkerOptions().position(beijing).title("Marker in WenDuShuiCheng"));
-        aMap.moveCamera(CameraUpdateFactory.newLatLng(beijing));
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // When the compile and target version is higher than 22, please request the
-        // following permissions at runtime to ensure the
-        // SDK work well.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
-                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
-                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
-                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
-                            Manifest.permission.READ_PHONE_STATE,
-                    }
-                    , 1);
-        }
-
-        setContentView(R.layout.activity_main);
-        mapView = (MapView) findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-        initMapView();
-        initUI();
-    }
-
-    private void showSettingDialog() {
-        LinearLayout wayPointSettings = (LinearLayout) getLayoutInflater().inflate(R.layout.dialog_waypointsetting, null);
-        final TextView wpAltitude_TV = (TextView) wayPointSettings.findViewById(R.id.altitude);
-        RadioGroup speed_RG = (RadioGroup) wayPointSettings.findViewById(R.id.speed);
-        RadioGroup actionAfterFinished_RG = (RadioGroup) wayPointSettings.findViewById(R.id.actionAfterFinished);
-        RadioGroup heading_RG = (RadioGroup) wayPointSettings.findViewById(R.id.heading);
-        speed_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                Log.d(TAG, "Select Speed finish");
-            }
-        });
-        actionAfterFinished_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // TODO Auto-generated method stub
-                Log.d(TAG, "Select action action");
-            }
-        });
-        heading_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // TODO Auto-generated method stub
-                Log.d(TAG, "Select heading finish");
-            }
-        });
-        new AlertDialog.Builder(this)
-                .setTitle("")
-                .setView(wayPointSettings)
-                .setPositiveButton("Finish", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int id) {
-
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int id) {
-                        dialogInterface.cancel();
-                    }
-                })
-                .create()
-                .show();
-    }
-
-    @Override
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
-        switch (v.getId()) {
-            case R.id.config:{
-                showSettingDialog();
-                break;
-            }
-            default:
-                break;
-        }
-    }
-    @Override
-    public void onMapClick(LatLng point) {
-    }
-
-}
+package com.stt.uavos;import android.Manifest;import android.app.AlertDialog;import android.content.DialogInterface;import android.os.Build;import android.os.Bundle;import android.support.v4.app.ActivityCompat;import android.support.v7.app.AppCompatActivity;import android.util.Log;import android.view.View;import android.widget.Button;import android.widget.LinearLayout;import android.widget.RadioGroup;import android.widget.TextView;import com.amap.api.maps2d.AMap;import com.amap.api.maps2d.CameraUpdateFactory;import com.amap.api.maps2d.MapView;import com.amap.api.maps2d.model.LatLng;import com.amap.api.maps2d.model.MarkerOptions;public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMap.OnMapClickListener{    protected static final String TAG = "MainActivity";    private MapView mapView;    private AMap aMap;    private Button locate, add, clear;    private Button config, upload, start, stop;    @Override    protected void onResume() {        super.onResume();    }    @Override    protected void onPause() {        super.onPause();    }    @Override    protected void onDestroy() {        super.onDestroy();    }    public void onReturn(View view) {        Log.d(TAG, "onReturn");        this.finish();    }    private void initUI() {        locate = (Button) findViewById(R.id.locate);        add = (Button) findViewById(R.id.add);        clear = (Button) findViewById(R.id.clear);        config = (Button) findViewById(R.id.config);        upload = (Button) findViewById(R.id.upload);        start = (Button) findViewById(R.id.start);        stop = (Button) findViewById(R.id.stop);        locate.setOnClickListener(this);        add.setOnClickListener(this);        clear.setOnClickListener(this);        config.setOnClickListener(this);        upload.setOnClickListener(this);        start.setOnClickListener(this);        stop.setOnClickListener(this);    }    private void initMapView() {        if(aMap == null) {            aMap = mapView.getMap();            aMap.setOnMapClickListener(this);        }        LatLng beijing = new LatLng(40.10080756, 116.37034178);//高德地图：116.37034178；40.10080756// 百度地图：40.106683, 116.376924        aMap.addMarker(new MarkerOptions().position(beijing).title("Marker in WenDuShuiCheng"));        aMap.moveCamera(CameraUpdateFactory.newLatLng(beijing));    }    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        // When the compile and target version is higher than 22, please request the        // following permissions at runtime to ensure the        // SDK work well.        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {            ActivityCompat.requestPermissions(this,                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,                            Manifest.permission.READ_PHONE_STATE,                    }                    , 1);        }        setContentView(R.layout.activity_main);        mapView = (MapView) findViewById(R.id.map);        mapView.onCreate(savedInstanceState);        initMapView();        initUI();    }    private void showSettingDialog() {        LinearLayout wayPointSettings = (LinearLayout) getLayoutInflater().inflate(R.layout.dialog_waypointsetting, null);        final TextView wpAltitude_TV = (TextView) wayPointSettings.findViewById(R.id.altitude);        RadioGroup speed_RG = (RadioGroup) wayPointSettings.findViewById(R.id.speed);        RadioGroup actionAfterFinished_RG = (RadioGroup) wayPointSettings.findViewById(R.id.actionAfterFinished);        RadioGroup heading_RG = (RadioGroup) wayPointSettings.findViewById(R.id.heading);        speed_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {            @Override            public void onCheckedChanged(RadioGroup radioGroup, int i) {                Log.d(TAG, "Select Speed finish");            }        });        actionAfterFinished_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {            @Override            public void onCheckedChanged(RadioGroup group, int checkedId) {                // TODO Auto-generated method stub                Log.d(TAG, "Select action action");            }        });        heading_RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {            @Override            public void onCheckedChanged(RadioGroup group, int checkedId) {                // TODO Auto-generated method stub                Log.d(TAG, "Select heading finish");            }        });        new AlertDialog.Builder(this)                .setTitle("")                .setView(wayPointSettings)                .setPositiveButton("Finish", new DialogInterface.OnClickListener() {                    @Override                    public void onClick(DialogInterface dialogInterface, int id) {                    }                })                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {                    @Override                    public void onClick(DialogInterface dialogInterface, int id) {                        dialogInterface.cancel();                    }                })                .create()                .show();    }    @Override    public void onClick(View v) {        // TODO Auto-generated method stub        switch (v.getId()) {            case R.id.config:{                showSettingDialog();                break;            }            default:                break;        }    }    @Override    public void onMapClick(LatLng point) {    }}
--- app/src/main/java/com/stt/uavos/UAVOSApplication.java
+++ app/src/main/java/com/stt/uavos/UAVOSApplication.java
@@ -1,16 +1,1 @@
-package com.stt.uavos;
-
-import android.app.Application;
-
-/**
- * Created by Administrator on 2017/7/7.
- */
-
-public class UAVOSApplication extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-}
+package com.stt.uavos;import android.app.Application;/** * Created by Administrator on 2017/7/7. */public class UAVOSApplication extends Application {    @Override    public void onCreate() {        super.onCreate();    }}
