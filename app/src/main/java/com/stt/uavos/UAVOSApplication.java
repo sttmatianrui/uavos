dc4b2ce2c5228eb7c75393db898f77686a752855
fengpy <fengpeiyun@safetytech.cn>
2017-07-10 13:28:27 +0800

    import jar

---
+++ app/src/main/java/com/stt/uavos/UAVOSApplication.java
@@ -0,0 +1,16 @@
+package com.stt.uavos;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/7/7.
+ */
+
+public class UAVOSApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
